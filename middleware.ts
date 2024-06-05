91005f5a0b610ca5831566ce340a69670331b9c6
Radostin Romanov <rad.romanov@gmail.com>
2024-06-05 11:09:33 +0300

    Add redirect middleware

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,24 @@
-import {
-  auth,
-  clerkMiddleware,
-  createRouteMatcher,
-} from "@clerk/nextjs/server";
-import { NextRequest, NextResponse } from "next/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-// Used to protect all routes, exclduing ones passed in the array
+// Used to protect all routes, excluding ones passed in the array
 const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
 
 // Used to protect routes specified in the array
-// const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
 
-export default clerkMiddleware((auth, req) => {
-  const { protect } = auth();
+export default clerkMiddleware(async (auth, req) => {
+  const { protect, userId } = auth();
 
   if (!isPublicRoute(req)) protect();
+
+  if (userId && !req.nextUrl.pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
 });
 
 export const config = {
   // The following matcher runs middleware on all routes
   // except static assets.
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
